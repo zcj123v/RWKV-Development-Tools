65d32595676c120b61a60d7f0e73293e3af63275
ssssg <870326915@qq.com>
2025-02-10 01:10:41 +0800

    modified:   playground_webui.py

--- playground_webui.py
+++ playground_webui.py
@@ -443,10 +443,10 @@ with gr.Blocks() as demo:
                 interactive=True,
                 visible=False,
             )
-            eval_mode_selector = gr.Dropdown(
-                choices=["随意回复", "选择题", "代码"],
+            bmk_mode_selector = gr.Dropdown(
+                choices=["自定义问题", "选择题", "代码"],
                 label="选择评估类型",
-                value="随意回复",
+                value="自定义问题",
             )
             bmk_using_init_state_checkbox.change(
                 lambda x: gr.update(visible=x),
@@ -742,7 +742,7 @@ with gr.Blocks() as demo:
                 )
 
             # 处理模式切换的函数
-            def switch_mode_eval(choice):
+            def switch_mode_bmk(choice):
                 if choice == "自定义问题":
                     return (
                         gr.Column(visible=True),
@@ -763,9 +763,9 @@ with gr.Blocks() as demo:
                     )
 
             # 绑定模式切换事件
-            eval_mode_selector.change(
-                fn=switch_mode_eval,
-                inputs=eval_mode_selector,
+            bmk_mode_selector.change(
+                fn=switch_mode_bmk,
+                inputs=bmk_mode_selector,
                 outputs=[
                     free_chat_section,
                     choice_question_section,
